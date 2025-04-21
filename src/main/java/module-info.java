e4bb8d9534072929d985ecbb845d9a8e856157f5
Evanesce <anthoniusevan@gmail.com>
2025-04-21 21:30:16 +1000

    Added RichTextFX and ability to format text
    Text can now be formatted by making a selection. Available options are bold, italic, and underline. Can also play around with font size. Overall, still glitchy and still need fixing and bug testing.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.staticconstants.flowpad.flowpad {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.fxmisc.richtext;
 
     opens com.staticconstants.flowpad to javafx.fxml;
     exports com.staticconstants.flowpad;
