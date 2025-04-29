7cfd2b0c47797960c625c1990d3e0991615572f1
Evanesce <anthoniusevan@gmail.com>
2025-04-29 20:49:53 +1000

    Added other test but not finished just yet. All test has passed

--- src/test/com/staticconstants/flowpad/frontend/MainEditorControllerTest.java
+++ src/test/com/staticconstants/flowpad/frontend/MainEditorControllerTest.java
@@ -1,27 +1,40 @@
 package com.staticconstants.flowpad.frontend;
 
+import javafx.application.Platform;
 import javafx.scene.control.Button;
 import org.junit.jupiter.api.*;
 
 import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainEditorControllerTest {
     private static final HashMap<String, String> STYLE_HASHMAP = new HashMap<String, String>();
     private static final String STYLE_STRING = "-fx-font-weight:bold;-fx-background-color:black;-fx-font-size:12px;";
+
+    @BeforeAll
+    public static void initJFX() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.startup(latch::countDown); // Only once per JVM
+        latch.await();
+    }
+
     @BeforeEach
     void setUp() {
         STYLE_HASHMAP.put("-fx-font-weight", "bold");
         STYLE_HASHMAP.put("-fx-background-color", "black");
         STYLE_HASHMAP.put("-fx-font-size", "12px");
-
-        MainEditorController controller = new MainEditorController();
     }
 
     @Test
     void testHashMapStyleToString(){
         String styleInString = MainEditorController.hashMapStyleToString(STYLE_HASHMAP);
-        assertEquals(STYLE_STRING, styleInString);
+//        Apparently doesn't return the keys and value in order when combined
+//        assertEquals(STYLE_STRING, styleInString);
+        for (String style : styleInString.split(";")){
+            assertTrue(STYLE_STRING.contains(style));
+        }
     }
 
     @Test
@@ -72,17 +85,23 @@ class MainEditorControllerTest {
         // not sure if this one is suitable for unit testing
     }
 
-    @Test
-    void testAddStyle(){
 
+    @Test
+    void testAddOrRemoveStyle(){
+        // work in progress, could not make it work if it's directly interacting with richtextfx
+        // prob going to minimize the scale of the function can call it from the low level process
     }
 
     @Test void testParseStyle(){
-
+        HashMap<String, String> result = MainEditorController.parseStyle(STYLE_STRING);
+        for (String key : STYLE_HASHMAP.keySet()){
+            assertEquals(STYLE_HASHMAP.get(key), result.get(key));
+        }
     }
 
     @Test void testGetStyleValue()
     {
-
+        String value = MainEditorController.getStyleValue(STYLE_STRING, "-fx-font-weight");
+        assertEquals("bold", value);
     }
 }
