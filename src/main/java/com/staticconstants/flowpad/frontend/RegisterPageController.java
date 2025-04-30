f99d91f97f7853a3c2dc71d84ba07295aac5d2dd
Alvin <alvinjohnson2310@gmail.com>
2025-04-30 18:07:00 +1000

    directing to correct pages

--- src/main/java/com/staticconstants/flowpad/frontend/RegisterPageController.java
+++ src/main/java/com/staticconstants/flowpad/frontend/RegisterPageController.java
@@ -61,7 +61,7 @@ public class RegisterPageController {
             alert.show();
 
             Stage stage = (Stage) btnRegister.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(FlowPadApplication.class.getResource("maineditor-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(FlowPadApplication.class.getResource("register-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             stage.setScene(scene);
             stage.show();
